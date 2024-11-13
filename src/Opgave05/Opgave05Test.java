5a8185a53c17d90644c9596078e57fef5de0a164
emilnordstroem <emilstoeve@outlook.com>
2024-11-13 15:18:45 +0100

    Update

--- src/Opgave05/Opgave05Test.java
+++ src/Opgave05/Opgave05Test.java
@@ -2,6 +2,8 @@ package Opgave05;
 
 public class Opgave05Test {
     public static void main(String[] args) {
-
+        int number = 200;
+        System.out.println(SearchAlgorithm.largestWholeNumberLinear(number));
+        System.out.println(SearchAlgorithm.largestWholeNumberBinary(number));
     }
 }
